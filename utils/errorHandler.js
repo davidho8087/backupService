ee065556e45f8e92c1e255ad5a4205c496800f53
David Ho <1073113@gmail.com>
2024-04-12 12:51:51 +0800

    Revert "first commit"
    This reverts commit 83230e159bbbb3b7b8003ffab1ed45243adf906d.

--- utils/errorHandler.js
+++
@@ -1,7 +0,0 @@
-import logger from '../lib/logger.js'
-
-export const handleError = (error, source) => {
-    logger.error(`Error Message in ${source}:`, error.message);
-    logger.error(`Full-Error in ${source}:`, error);
-}
-
